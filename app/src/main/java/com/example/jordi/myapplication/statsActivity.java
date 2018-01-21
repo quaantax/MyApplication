934f89a43d7feb0c63367112ee2ac46d8122b2d3
Jordi <jordijedi@gmail.com>
2018-01-21 22:56:42 +0100

    pruebamecagoendios

--- app/src/main/java/com/example/jordi/myapplication/statsActivity.java
+++ app/src/main/java/com/example/jordi/myapplication/statsActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.CancellationSignal;
+import android.os.Handler;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,42 +25,68 @@ import java.io.InputStream;
 
 
 public class statsActivity extends AppCompatActivity {
+    private EditText editText;
+    ProgressDialog mProgressDialog;
+    Handler mHandler;
+    String statsloader="";
+    conexion cn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        editText=(EditText)findViewById(R.id.editText);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        conexion cn= new conexion(getApplicationContext(),"76561198074289873");
 
         //Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-
-        // Instantiate the RequestQueue.
-        final TextView mTextView = (TextView) findViewById(R.id.text);
-
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=E590B7B42BC693F80DA538D6DC7DA98F&steamid="+message;
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        mTextView.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work!");
-            }
-        });
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        launchProgressDialog();
 
         }
 
+    public void launchProgressDialog() {
+        mProgressDialog = new ProgressDialog(statsActivity.this);
+        mProgressDialog.setMessage("Loading ...");
+        mProgressDialog.setProgressStyle(mProgressDialog.STYLE_SPINNER);
+        mProgressDialog.setProgress(0);
+        mProgressDialog.setMax(100);
+        mProgressDialog.show();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    while(statsloader.equals("")){
+                        Thread.sleep(1000);
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                statsloader = cn.apistats();
+                                if(statsloader!=""){
+                                    mProgressDialog.dismiss();
+                                    /*if(statsloader.getText().toString().equals(statsloader)){
+
+                                    }else if(!edPassword.getText().toString().equals(statsloader)){
+                                        Toast.makeText(getApplicationContext(), "ID incorrecto", Toast.LENGTH_SHORT).show();
+                                    }*/
+                                    editText.setText(statsloader);
+                                    }
+
+                            }
+                        });
+
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
 
 
     }
